f8d283cd0b3b613dd7c08e54917365b820653bfc
Rashaad Mirza <54775524+rashaadmirza@users.noreply.github.com>
2026-04-26 17:22:23 -0600

    initial commit

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import streamlit as st
 
-COMPANY_ACCOUNT_ID = "100093712"
+COMPANY_ACCOUNT_ID = st.secrets["COMPANY_ACCOUNT_ID"]
 
 def get_output_filename():
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
